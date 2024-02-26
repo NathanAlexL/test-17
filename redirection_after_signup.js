7e2c43d94c3040d563e8f15c34942bf07844f021
NathanAlexL <161165627+NathanAlexL@users.noreply.github.com>
2024-02-26 10:37:35 +0400

    Create redirection_after_signup.js*

---
+++ redirection_after_signup.js
@@ -0,0 +1,5 @@
+// Redirection après création du compte
+function redirectToDashboard() {
+    // Simuler une redirection vers le tableau de bord de l'utilisateur
+    window.location.href = "dashboard.html";
+}
