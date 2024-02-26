4c2e1b35970a4c40a60621fdec22cba534277d33
NathanAlexL <161165627+NathanAlexL@users.noreply.github.com>
2024-02-26 10:36:14 +0400

    Create redirect_after_login.js

---
+++ redirect_after_login.js
@@ -0,0 +1,5 @@
+// Script de redirection après connexion
+function redirectToDashboard() {
+    // Simuler une redirection vers le tableau de bord de l'utilisateur
+    window.location.href = "dashboard.html";
+}
